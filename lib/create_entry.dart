16c27a46638ff43b5fe82bc0eb9ad53ddfc13b85
chop_me <ryo.nakai0@gmail.com>
2020-05-02 14:38:18 +0900

    on create entry page, wrapped with padding widget

--- lib/create_entry.dart
+++ lib/create_entry.dart
@@ -16,32 +16,35 @@ class _CreateEntryState extends State<CreateEntry> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("title"),),
-        body: Column(
-          children: <Widget>[
-            Text(strDate),
-            Text("please enter new entry TITLE"),
-            TextField(
-              onChanged: (entryTitle){
-                titleText = entryTitle;
-              },
-            ),
-            Text("please enter new article"),
-            TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              onChanged: (article){
-                articleText = article;
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(strDate),
+              Text("please enter new entry TITLE"),
+              TextField(
+                onChanged: (entryTitle){
+                  titleText = entryTitle;
+                },
+              ),
+              Text("please enter new article"),
+              TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                onChanged: (article){
+                  articleText = article;
 //                setState(() {
 //                });
-              },
-            ),
-            FlatButton(
-              child: Icon(Icons.add_circle),
-              onPressed: (){
-                print(articleText);
-              },
-            )
-          ],
+                },
+              ),
+              FlatButton(
+                child: Icon(Icons.add_circle),
+                onPressed: (){
+                  print(articleText);
+                },
+              )
+            ],
+          ),
         ));
   }
 }
