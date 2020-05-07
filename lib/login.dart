c5afdf11149934280dd2d1291a4d9c80862a6ca1
chop_me <ryo.nakai0@gmail.com>
2020-05-07 16:38:38 +0900

    enabled obscureText

--- lib/login.dart
+++ lib/login.dart
@@ -20,14 +20,17 @@ class _LoginEntryState extends State<LoginPage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: <Widget>[
+              SizedBox(height: 20,),
               Text("please enter your login ID"),
               TextField(
                 onChanged: (loginName){
                   LoginID = loginName;
                 },
               ),
+              SizedBox(height: 20,),
               Text("please enter your password"),
               TextField(
+                obscureText: true,
                 onChanged: (pass){
                   password = pass;
                 },
