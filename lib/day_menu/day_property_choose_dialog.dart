14147e9f718e1a67018cf6148b5fbf4001e82d69
chop_me <ryo.nakai0@gmail.com>
2020-05-28 18:11:24 +0900

    greenを削除

--- lib/day_menu/day_property_choose_dialog.dart
+++ lib/day_menu/day_property_choose_dialog.dart
@@ -9,30 +9,26 @@ class DayPropertyChooseDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(title: Text("title"), children: <Widget>[
-      SimpleDialogOption(
-        onPressed: () {
-          addDayProperty(Colors.red);
-          Navigator.pop(context);
-          navigatePage(context, InputTextProperty());
-        },
-        child: Text("red article"),
-      ),
-      SimpleDialogOption(
-        onPressed: () {
-          addDayProperty(Colors.blue);
-          Navigator.pop(context);
-          navigatePage(context, InputImageProperty());
-        },
-        child: Text("blue image"),
-      ),
-      SimpleDialogOption(
-        onPressed: () {
-          addDayProperty(Colors.green);
-          Navigator.pop(context);
-        },
-        child: Text("green"),
-      ),
-    ]);
+    return SimpleDialog(
+      title: Text("title"),
+      children: <Widget>[
+        SimpleDialogOption(
+          onPressed: () {
+            addDayProperty(Colors.red);
+            Navigator.pop(context);
+            navigatePage(context, InputTextProperty());
+          },
+          child: Text("red article"),
+        ),
+        SimpleDialogOption(
+          onPressed: () {
+            addDayProperty(Colors.blue);
+            Navigator.pop(context);
+            navigatePage(context, InputImageProperty());
+          },
+          child: Text("blue image"),
+        ),
+      ],
+    );
   }
 }
