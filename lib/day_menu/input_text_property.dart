06e62c44b4602f45d0afe06d990335e7257f29cd
chop_me <ryo.nakai0@gmail.com>
2020-05-28 17:24:03 +0900

    text property作成画面にタイトル入力を追加

--- lib/day_menu/input_text_property.dart
+++ lib/day_menu/input_text_property.dart
@@ -12,23 +12,37 @@ class _InputTextPropertyState extends State<InputTextProperty> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("article")),
-        body: Column(
-          children: <Widget>[
-            SizedBox(height: 20.0,),
-            TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                onChanged: (article) {
-                  articleText = article;
-                }
-            ),
-            FlatButton(
-              child: Icon(Icons.add_circle),
-              onPressed: () {
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'enter your title'
+                  ),
+              ),
+              SizedBox(height: 20.0,),
+              TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'enter your article'
+                  ),
+                  onChanged: (article) {
+                    articleText = article;
+                  }
+              ),
+              FlatButton(
+                child: Icon(Icons.add_circle),
+                onPressed: () {
 
-              },
-            )
-          ],
+                },
+              )
+            ],
+          ),
         ));
   }
 }
