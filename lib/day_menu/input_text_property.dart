4e1600f8cedc8cc7ff75ea197fd839fcc39e3219
chop_me <ryo.nakai0@gmail.com>
2020-07-04 14:23:10 +0900

    画面の表示崩れを直した　/　テキストフィールドの枠を取った　／　タイトルサイズの変更

--- lib/day_menu/input_text_property.dart
+++ lib/day_menu/input_text_property.dart
@@ -2,7 +2,6 @@ import 'package:comfortable_diary/functions.dart';
 import 'package:flutter/material.dart';
 
 class InputTextProperty extends StatefulWidget {
-
   @override
   _InputTextPropertyState createState() => _InputTextPropertyState();
 }
@@ -14,8 +13,9 @@ class _InputTextPropertyState extends State<InputTextProperty> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("article")),
-        body: Padding(
+      appBar: AppBar(title: Text("article")),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: <Widget>[
@@ -23,41 +23,45 @@ class _InputTextPropertyState extends State<InputTextProperty> {
                 height: 20.0,
               ),
               TextField(
+                  style: TextStyle(
+                    fontSize: 25.0,
+                  ),
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'enter your title'),
+
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: 'title'),
                   onChanged: (title) {
                     articleTitle = title;
                   }),
-              SizedBox(
-                height: 20.0,
-              ),
+//              SizedBox(
+//                height: 20.0,
+//              ),
+              Divider(height: 10.0, thickness: 2.0,),
               TextField(
                   keyboardType: TextInputType.multiline,
                   maxLines: null,
                   decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'enter your article'),
+                      border: OutlineInputBorder(borderSide: BorderSide.none),
+                      hintText: 'enter your article'),
                   onChanged: (article) {
                     articleText = article;
                   }),
-              FlatButton(
-                child: Icon(Icons.add_circle),
-                onPressed: () {
-                  try {
-                    createTextProperty(articleTitle, articleText);
-                    Navigator.pop(context);
-                  }
-                  catch (error) {
-                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("error!")));
-                  }
-//                  widget.addDayProperty(Property(propertyType.text,
-//                      title: articleTitle, body: articleText));
-//                  createDiary("articles", articleTitle, articleText);
-                },
-              )
             ],
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          try {
+            createTextProperty(articleTitle, articleText);
+            Navigator.pop(context);
+          } catch (error) {
+            Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text("error!")));
+          }
+        },
+      ),
+    );
   }
 }
